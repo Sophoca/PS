bada3e919bfa81c86705d5262bfaecde0e73c827
Sophoca <tjdqls1668@naver.com>
2022-03-03 23:20:22 +0900

    2022-03-03
    11052: dp
    1676: math
    1991: tree

--- 백준/1991.py
+++ 백준/1991.py
@@ -11,3 +11,44 @@ class Node:
         self.left = None
         self.right = None
 
+    def __str__(self):
+        return self.data
+
+    def pre_print(self):
+        print(self.data, end="")
+        if self.left:
+            tree[self.left].pre_print()
+        if self.right:
+            tree[self.right].pre_print()
+
+    def middle_print(self):
+        if self.left:
+            tree[self.left].middle_print()
+        print(self.data, end="")
+        if self.right:
+            tree[self.right].middle_print()
+
+    def post_print(self):
+        if self.left:
+            tree[self.left].post_print()
+        if self.right:
+            tree[self.right].post_print()
+        print(self.data, end="")
+
+
+N = int(input())
+tree = dict()
+for _ in range(N):
+    a, b, c = map(str, input().split())
+    node = Node(a)
+    if b != ".":
+        node.left = b
+    if c != ".":
+        node.right = c
+    tree[a] = node
+
+tree["A"].pre_print()
+print()
+tree["A"].middle_print()
+print()
+tree["A"].post_print()
