0dfb22a766a40b150dfdfcb4d189b25127f19d90
Sophoca <tjdqls1668@naver.com>
2022-10-02 03:06:00 +0900

    21608

--- 백준/21608.py
+++ 백준/21608.py
@@ -1,31 +1,77 @@
+# start -> 2:05, end -> 3:03
 import sys
+from collections import defaultdict
 
 input = sys.stdin.readline
 
 
-def get_adjacent_index(r, c):
-    indexes = []
-    if r - 1 >= 0:
-        if c - 1 >= 0:
-            indexes.append(answers[r - 1][c - 1])
-        elif c + 1 < N:
-            indexes.append(answers[r - 1][c + 1])
-    elif r + 1 < N:
-        if c - 1 >= 0:
-            indexes.append(answers[r + 1][c - 1])
-        elif c + 1 < N:
-            indexes.append(answers[r + 1][c + 1])
+def find_adjacent(row, col):
+    result = set()
+    for i in range(4):
+        if 0 <= row + dY[i] < N and 0 <= col + dX[i] < N:
+            result.add((row + dY[i], col + dX[i]))
+    return result
+
+
+def find_favorite_cell(student_num):
+    candidates = defaultdict(lambda: [])
+    for row in range(N):
+        for col in range(N):
+            if answer[row][col] == 0:
+                temp_coord = find_adjacent(row, col)
+                count = 0
+                for temp_r, temp_c in temp_coord:
+                    if answer[temp_r][temp_c] in students[student_num]:
+                        count += 1
+                candidates[count].append((row, col))
+    return sorted(candidates.items(), key=lambda item: item[0], reverse=True)[0]
+
+
+def find_blank_adjacent_cell(candidate_coords):
+    candidates = defaultdict(lambda: [])
+    for row, col in candidate_coords:
+        temp_coord = find_adjacent(row, col)
+        count = 0
+        for temp_r, temp_c in temp_coord:
+            if answer[temp_r][temp_c] == 0:
+                count += 1
+        candidates[count].append((row, col))
+    return sorted(candidates.items(), key=lambda item: item[0], reverse=True)[0]
+
+
+def calc_score():
+    score = 0
+    score_table = [0, 1, 10, 100, 1000]
+    for row in range(N):
+        for col in range(N):
+            candidates = find_adjacent(row, col)
+            count = 0
+            for temp_r, temp_c in candidates:
+                if answer[temp_r][temp_c] in students[answer[row][col]]:
+                    count += 1
+            score += score_table[count]
+    return score
 
 
 N = int(input())
-params = list(list(map(int, input().split(' '))) for _ in range(N ** 2))
+answer = [[0] * N for _ in range(N)]
+dX = [-1, 0, 1, 0]
+dY = [0, -1, 0, 1]
+
 students = dict()
-answers = list([0] * N for _ in range(N))
+for _ in range(N ** 2):
+    temp = list(map(int, input().split()))
+    students[temp[0]] = set(temp[1:])
 
-for param in params:
-    students[param[0]] = param[1:]
+for key, values in students.items():
+    favorite_friends = find_favorite_cell(key)
+    if len(favorite_friends[1]) == 0:
+        r, c = favorite_friends[1][0]
+        answer[r][c] = key
+        print(answer)
+        continue
+    blank_cells = find_blank_adjacent_cell(favorite_friends[1])
+    r, c = blank_cells[1][0]
+    answer[r][c] = key
 
-
-print(students, answers)
-
-# for key, value in students.items()
+print(calc_score())
